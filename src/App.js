20aace5a421b93607af60d9cb59c5dc77291fa34
Jason Nguyen <jtsnguyen621@gmail.com>
2018-11-06 17:12:30 -0800

    starting on retrieving gmaps info

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import { google_api_key } from './config';
 
+var geohash = require('ngeohash');
+var https = require('https');
+
 class App extends Component {
 
-  state = { url : "https://www.google.com/maps/embed/v1/place?q=...&key=" + google_api_key }
+
+  state = {
+    embed_url : "https://www.google.com/maps/embed/v1/place?q=...&key=" + google_api_key,
+    json_url: "https://maps.googleapis.com/maps/api/geocode/json?address=...&key=" + google_api_key,
+    loc : "",
+    geodata: {}
+  }
 
   render() {
     return (
@@ -13,9 +22,9 @@ class App extends Component {
           <h2>Geohash Thingy</h2>
           <div display="inline">
             <input name="new" type="text" value={this.state.value}
-            onChange={this.handleChange.bind(this)}
+            onChange={this.locUpdate.bind(this)}
             ></input>
-            <button onClick={this.clickHandle.bind(this)}>Go!</button>
+            <button onClick={this.locQuery.bind(this)}>Go!</button>
           </div>
           <p name="place">Please Select a Place</p>
           <iframe
@@ -24,19 +33,43 @@ class App extends Component {
             height="450"
             frameBorder="0"
             style={{border:0}}
-            src={this.state.url}
+            src={this.state.embed_url}
             allowFullScreen>
           </iframe>
         </header>
       </div>
     );
   }
-  handleChange(e){
-    this.setState({ value: e.target.value });
+  locUpdate(e){
+    this.setState({ loc: e.target.value });
     console.log(this.state);
   }
-  clickHandle(place){;
-    this.setState({url: "https://www.google.com/maps/embed/v1/place?q="+this.state.value+"&key=" + google_api_key});
+
+  geoUpdate(e){
+    this.updateMap(e);
+    this.locQuery(e);
+  }
+
+  updateMap(e){
+    this.setState({embed_url: "https://www.google.com/maps/embed/v1/place?q="+this.state.loc+"&key=" + google_api_key});
+  }
+  locQuery(e){
+    this.state.json_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + this.state.loc + "&key=" + google_api_key
+    var geodata = {};
+    https.get(this.state.json_url, (response) => {
+      var data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      response.on('end', ()=> {
+        geodata = JSON.parse(data);
+        console.log(geodata);
+
+      });
+    }).on("error", (err) => {
+      console.log(err.message);
+    });
   }
 }
 

5ed074fcaea154d1703b3ebf5e75f99cf0b9b771
Jason Nguyen <jtsnguyen621@gmail.com>
2018-11-06 16:33:16 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { google_api_key } from './config';
 
 class App extends Component {
+
+  state = { url : "https://www.google.com/maps/embed/v1/place?q=...&key=" + google_api_key }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Geohash Thingy</h2>
+          <div display="inline">
+            <input name="new" type="text" value={this.state.value}
+            onChange={this.handleChange.bind(this)}
+            ></input>
+            <button onClick={this.clickHandle.bind(this)}>Go!</button>
+          </div>
+          <p name="place">Please Select a Place</p>
+          <iframe
+            title="maps"
+            width="600"
+            height="450"
+            frameBorder="0"
+            style={{border:0}}
+            src={this.state.url}
+            allowFullScreen>
+          </iframe>
         </header>
       </div>
     );
+  }
+  handleChange(e){
+    this.setState({ value: e.target.value });
+    console.log(this.state);
+  }
+  clickHandle(place){;
+    this.setState({url: "https://www.google.com/maps/embed/v1/place?q="+this.state.value+"&key=" + google_api_key});
   }
 }
 
---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const google_api_key = process.env.REACT_APP_GOOGLE_API_KEY;
+console.log(google_api_key);

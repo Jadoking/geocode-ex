f1bf31d60d154decf1e13b6ee8e8945db02cd72c
Jason Nguyen <jtsnguyen621@gmail.com>
2018-11-07 17:13:02 -0800

    began working on geohash address lookup

--- src/App.js
+++ src/App.js
@@ -9,24 +9,55 @@ class App extends Component {
 
 
   state = {
-    embed_url : "https://www.google.com/maps/embed/v1/place?q=...&key=" + google_api_key,
-    json_url: "https://maps.googleapis.com/maps/api/geocode/json?address=...&key=" + google_api_key,
-    loc : "",
-    geodata: {}
+    embed_url : encodeURI("https://www.google.com/maps/embed/v1/place?q=...&key=" + google_api_key),
+    json_url: encodeURI("https://maps.googleapis.com/maps/api/geocode/json?address=...&key=" + google_api_key),
+    loc_input: '',
+    loc : '',
+    geodata: {},
+    geohash_input: '',
+    geohash: ''
   }
 
   render() {
+    let geodiv, hash;
+    if (this.state.geodata.status === "OK" && this.state.geodata.results) {
+      geodiv = <div>
+        { this.state.geodata.results[0].formatted_address }
+      </div>;
+      hash = <div>
+        Geohash: { this.state.geohash }
+      </div>;
+    }
+    //console.log(geodiv);
     return (
       <div className="App">
         <header className="App-header">
           <h2>Geohash Thingy</h2>
-          <div display="inline">
-            <input name="new" type="text" value={this.state.value}
-            onChange={this.locUpdate.bind(this)}
-            ></input>
-            <button onClick={this.geoUpdate.bind(this)}>Go!</button>
+          <div width="125px">
+            <div style={{"font-size":"10px","line-height":"1px","text-align":"left"}}>
+            Place name
+            </div>
+            <div display="inline">
+              <input name="place" type="text" value={this.state.value}
+              onChange={this.locUpdate.bind(this)}
+              ></input>
+              <button onClick={this.geoUpdate.bind(this)}>Go!</button>
+            </div>
           </div>
-          <p name="place">Please Select a Place</p>
+          <div width="125px">
+            <div style={{"font-size":"10px","line-height":"1px","text-align":"left","margin-top":"10px"}}>
+            Geohash
+            </div>
+            <div display="inline">
+              <input name="geohash" type="text" value={this.state.value}
+              onChange={this.hashUpdate.bind(this)}
+              ></input>
+              <button onClick={this.geoUpdate.bind(this)}>Go!</button>
+            </div>
+          </div>
+          <p name="place">Please Select a Place or Input a Geohash</p>
+          {geodiv}
+          {hash}
           <iframe
             title="maps"
             width="600"
@@ -40,22 +71,42 @@ class App extends Component {
       </div>
     );
   }
+  stateReset(full) {
+    var init_state = {
+      embed_url : encodeURI("https://www.google.com/maps/embed/v1/place?q=...&key=" + google_api_key),
+      json_url: encodeURI("https://maps.googleapis.com/maps/api/geocode/json?address=...&key=" + google_api_key),
+      loc: '',
+      geodata: {},
+      geohash: ''
+    };
+
+    if (full) {
+      init_state.loc_input = '';
+      init_state.geohash = '';
+    }
+
+    this.setState(init_state);
+  }
   locUpdate(e){
-    this.setState({ loc: e.target.value });
+    this.setState({ loc_input: e.target.value });
+  }
+  hashUpdate(e){
+    this.setState({ geohash_input: e.target.value });
     console.log(this.state);
   }
-
   geoUpdate(e){
+    this.stateReset();
     this.updateMap(e);
     this.locQuery(e);
   }
   updateMap(e){
-    this.setState({embed_url: "https://www.google.com/maps/embed/v1/place?q="+this.state.loc+"&key=" + google_api_key});
+    var embed_url = encodeURI("https://www.google.com/maps/embed/v1/place?q="+this.state.loc_input +"&key=" + google_api_key);
+    this.setState({embed_url: embed_url});
   }
   locQuery(e){
-    this.state.json_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + this.state.loc + "&key=" + google_api_key
+    var json_url = encodeURI("https://maps.googleapis.com/maps/api/geocode/json?address=" + this.state.loc_input + "&key=" + google_api_key);
     var geodata = {};
-    https.get(this.state.json_url, (response) => {
+    https.get(json_url, (response) => {
       var data = '';
       response.on('data', (chunk) => {
         data += chunk;
@@ -63,7 +114,22 @@ class App extends Component {
 
       response.on('end', ()=> {
         geodata = JSON.parse(data);
-        console.log(geodata);
+
+        if (geodata.status === "OK" && geodata.results) {
+          var suspect_loc = geodata.results[0];
+          var coordinates = suspect_loc.geometry.location;
+          //console.log(coordinates);
+          var newhash = geohash.encode(coordinates.lat, coordinates.lng, 12);
+          var address = {};
+
+          this.setState({
+            json_url: json_url,
+            geodata: geodata,
+            geohash: newhash
+          });
+          console.log(this.state);
+        }
+        //console.log(geodata);
 
       });
     }).on("error", (err) => {
@@ -71,5 +137,7 @@ class App extends Component {
     });
   }
 }
+
+
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
 export const google_api_key = process.env.REACT_APP_GOOGLE_API_KEY;
-console.log(google_api_key);
